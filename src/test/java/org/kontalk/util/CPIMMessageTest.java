5d00a4f3f85a02aebf6b87dc71e82c7f3f27a949
Daniele Ricci <daniele.athome@gmail.com>
2016-03-06 16:10:16 +0100

    CPIM: support for multiple recipients
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

--- src/test/java/org/kontalk/util/CPIMMessageTest.java
+++ src/test/java/org/kontalk/util/CPIMMessageTest.java
@@ -32,7 +32,7 @@ import static org.junit.Assert.*;
 public class CPIMMessageTest {
 
     private static final String FROM = "alice@example.com";
-    private static final String TO = "bob@example.com";
+    private static final String[] TO = { "alice@example.com", "bob@example.com" };
     private static final Date DATE = new Date(1408912505000L);
     private static final String MIME = "text/plain";
     private static final String BODY = "TEST BODY";
@@ -41,7 +41,7 @@ public class CPIMMessageTest {
         "Content-type: Message/CPIM\n"+
         "\n" +
         "From: alice@example.com\n" +
-        "To: bob@example.com\n" +
+        "To: alice@example.com; bob@example.com\n" +
         "DateTime: 2014-08-24T20:35:05+00:00\n" +
         "\n" +
         "Content-type: text/plain; charset=utf-8\n" +
@@ -64,7 +64,7 @@ public class CPIMMessageTest {
     public void testParse() throws Exception {
         CPIMMessage m = CPIMMessage.parse(OUTPUT);
         assertEquals("From attribute not matching.", FROM, m.getFrom());
-        assertEquals("To attribute not matching.", TO, m.getTo());
+        assertArrayEquals("To attribute not matching.", TO, m.getTo());
         assertEquals("Mime attribute not matching.", MIME, m.getMime());
         assertEquals("Date attribute not matching.", DATE, m.getDate());
         assertEquals("Body not matching.", BODY, m.getBody());
