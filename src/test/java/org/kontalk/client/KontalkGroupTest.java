0672ffc6bd4b8ed8b369ecda057318d8a47f05c9
Daniele Ricci <daniele.athome@gmail.com>
2017-09-02 17:27:41 +0200

    Some tests
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ src/test/java/org/kontalk/client/KontalkGroupTest.java
@@ -0,0 +1,71 @@
+/*
+ * Kontalk client common library
+ * Copyright (C) 2017 Kontalk Devteam <devteam@kontalk.org>
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.kontalk.client;
+
+import java.util.Collections;
+
+import org.jivesoftware.smack.packet.Message;
+import org.junit.Before;
+import org.junit.Test;
+import org.jxmpp.jid.impl.JidCreate;
+
+import static org.junit.Assert.*;
+
+
+public class KontalkGroupTest {
+
+    private GroupExtension.Provider provider;
+
+    @Before
+    public void setUp() {
+        provider = new GroupExtension.Provider();
+    }
+
+    @Test
+    public void testCheckRequest() throws Exception {
+        GroupExtension ext;
+        KontalkGroupManager.KontalkGroup group;
+        Message cmd;
+
+        ext = new GroupExtension("mad-group", "david@localhost", GroupExtension.Type.CREATE,
+            "Mad group", Collections.<GroupExtension.Member>emptyList());
+        ext.addMember("alpha@localhost");
+        ext.addMember("beta@localhost");
+
+        cmd = new Message(JidCreate.from("golia@localhost"));
+        cmd.setFrom(JidCreate.from("charie@localhost"));
+        cmd.addExtension(ext);
+
+        group = new KontalkGroupManager.KontalkGroup(null, "mad-group", "david@localhost");
+        assertFalse(group.checkRequest(cmd));
+
+        ext = new GroupExtension("mad-group", "david@localhost", GroupExtension.Type.SET,
+            "Mad group", Collections.<GroupExtension.Member>emptyList());
+        ext.addMember("alpha@localhost");
+        ext.addMember("beta@localhost");
+
+        cmd = new Message(JidCreate.from("golia@localhost"));
+        cmd.setFrom(JidCreate.from("charie@localhost"));
+        cmd.addExtension(ext);
+
+        group = new KontalkGroupManager.KontalkGroup(null, "mad-group", "david@localhost");
+        assertFalse(group.checkRequest(cmd));
+    }
+
+}
